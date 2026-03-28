a79968ee91fa89e6b979e0bbff7fc0886a26777b
Belousov <Belousov@DESKTOP-33C3Q0P>
2026-03-28 21:58:22 +0500

    Добавьте файлы проекта.

---
+++ CaesarCipher.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipherProg
+{
+    internal class CaesarCipher
+    {
+        static string lowerRu = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        static string upperRu = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        public static string Encrypt(string text, int shift)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char letter = text[i];
+
+                if (upperRu.IndexOf(letter) != -1)
+                {
+                    int index = (upperRu.IndexOf(letter) + shift) % 33;
+                    letter = upperRu[index];
+                }
+                else if (lowerRu.IndexOf(letter) != -1)
+                {
+                    int index = (lowerRu.IndexOf(letter) + shift) % 33;
+                    letter = lowerRu[index];
+                }
+                else if (letter >= 'a' && letter <= 'z')
+                {
+                    int index = (letter - 'a' + shift) % 26;
+                    letter = (char)('a' + index);
+                }
+                else if (letter >= 'A' && letter <= 'Z')
+                {
+                    int index = (letter - 'A' + shift) % 26;
+                    letter = (char)('A' + index);
+                }
+
+                result = result + letter;
+            }
+
+            return result;
+        }
+
+        public static string Decrypt(string text, int shift)
+        {
+            int reverseShiftRu = 33 - shift % 33;
+            int reverseShiftEn = 26 - shift % 26;
+
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char letter = text[i];
+
+                if (upperRu.IndexOf(letter) != -1)
+                {
+                    int index = (upperRu.IndexOf(letter) + reverseShiftRu) % 33;
+                    letter = upperRu[index];
+                }
+                else if (lowerRu.IndexOf(letter) != -1)
+                {
+                    int index = (lowerRu.IndexOf(letter) + reverseShiftRu) % 33;
+                    letter = lowerRu[index];
+                }
+                else if (letter >= 'a' && letter <= 'z')
+                {
+                    int index = (letter - 'a' + reverseShiftEn) % 26;
+                    letter = (char)('a' + index);
+                }
+                else if (letter >= 'A' && letter <= 'Z')
+                {
+                    int index = (letter - 'A' + reverseShiftEn) % 26;
+                    letter = (char)('A' + index);
+                }
+
+                result = result + letter;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using CaesarCipherProg;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Привет! Это программа для шифра Цезаря.");
+        Console.WriteLine("Введите текст который хотите зашифровать или расшифровать:");
+        string text = Console.ReadLine();
+
+        Console.WriteLine("Введите сдвиг от 1 до 32:");
+        int shift;
+        while (!int.TryParse(Console.ReadLine(), out shift) || shift < 1 || shift > 25)
+        {
+            Console.WriteLine("Неверный ввод. Введите число от 1 до 32:");
+        }
+
+        Console.WriteLine("Что сделать? Введите 1 чтобы зашифровать, 2 чтобы расшифровать:");
+        int choice;
+        while (!int.TryParse(Console.ReadLine(), out choice) || (choice != 1 && choice != 2))
+        {
+            Console.WriteLine("Неверный ввод. Введите 1 или 2:");
+        }
+
+        string result = "";
+
+        if (choice == 1)
+        {
+            result = CaesarCipher.Encrypt(text, shift);
+            Console.WriteLine("Зашифрованный текст: " + result);
+        }
+        else
+        {
+            result = CaesarCipher.Decrypt(text, shift);
+            Console.WriteLine("Расшифрованный текст: " + result);
+        }
+    }
+}
