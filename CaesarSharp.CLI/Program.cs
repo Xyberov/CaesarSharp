d5c36ff9afdcd75768f84eace6187e91cb2d72ed
Xyberov <albel1603@yandex.ru>
2026-04-17 16:50:54 +0500

    Начал писать CLI

--- CaesarSharp.CLI/Program.cs
+++ CaesarSharp.CLI/Program.cs
@@ -1,2 +1,40 @@
-﻿using System.CommandLine;
+using System;
+using System.CommandLine;
+using System.IO;
 using CaesarSharp.Core;
+
+namespace CaesarSharp.CLI
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            var langOption = new Option<string>("--lang")
+            {
+                Description = $"Язык текста. Доступные: {string.Join(", ", Enum.GetNames(typeof(Language)))}.",
+                Required = true
+            };
+
+            var inputOption = new Option<string?>("--input")
+            {
+                Description = "Входной текст."
+            };
+
+            var fileOption = new Option<string?>("--file")
+            {
+                Description = "Путь к входному файлу."
+            };
+
+            var outputOption = new Option<string?>("--output")
+            {
+                Description = "Путь к выходному файлу (если не указан — вывод в консоль)."
+            };
+
+            var keyOption = new Option<int>("--key")
+            {
+                Description = "Сдвиг для шифрования/дешифрования.",
+                Required = true
+            };
+        }
+    }
+}
